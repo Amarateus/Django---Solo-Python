5c181a5b7f9356692ec58b7fd3203c5c59590142
Amarateus <98360631+Amarateus@users.noreply.github.com>
2022-01-25 20:09:56 -0300

    ok

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateViews
 
 app_name='blog'
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('create/', BlogCreateViews.as_view(), name='create')
 ]
